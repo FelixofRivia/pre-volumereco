7b2a1dc2f29efc65036d1085400719b084e0e786
FelixofRivia <filippomei99@gmail.com>
2025-05-03 15:20:15 +0200

    first commit

---
+++ prepare_input_data.py
@@ -0,0 +1,64 @@
+import numpy as np
+import drdf
+import h5py
+from pathlib import Path
+import sys
+sys.path.append('../tools_new/display')
+from read_mctruth import loadPrimariesEdepSim
+from geom_import import load_geometry
+from geometry import Geometry
+sys.path.append('../tools_new/analysis')
+from edepsim_deposits import EdepSimDeposits
+
+
+def get_response_data(event: int) -> np.ndarray:
+  assert len(event.items())==60, "Number of cameras is not 60"
+  cameras = np.empty((60, 32, 32, 2), dtype=np.float32)
+  for i, (cam, img) in enumerate(event.items()):
+    #print("cam :", cam)
+    #print("pixel shape:", np.array(img.pixels, np.float32).shape)
+    assert np.array(img.pixels, np.float32).shape == (32,32,2), "Camera shape is not (32,32,2)"  
+    cameras[i] = np.array(img.pixels, np.float32)
+  return cameras
+
+def get_truth_data(edepFile: str, geom: Geometry, event: int) -> np.ndarray:
+  MCtruth = loadPrimariesEdepSim(edepFile, event)
+  s = EdepSimDeposits(MCtruth, geom)
+  s.voxelize()
+  return s.voxels.voxels
+
+if __name__ == "__main__":
+
+  edepFile = "/home/filippo/DUNE/data/numu-CC-QE/skimmed-events-in-GRAIN_LAr_merged_reindex.edep-sim.root"
+  defs = {}
+  defs['voxel_size'] = 300
+  geometryPath = "/home/filippo/DUNE/GEOMETRIES/GRAIN_official"        #path to GRAIN geometry
+  geom = load_geometry(geometryPath, defs)
+
+  response_base_path = Path('/home/filippo/DUNE/data/numu-CC-QE/detector_response')
+  drdf_files = [f.name for f in response_base_path.glob('*.drdf') if f.is_file()]
+
+  print(drdf_files)
+
+  events = []
+  truths = []
+  for file in drdf_files:
+    reader = drdf.DRDF()
+    reader.read(response_base_path / file)
+    for run in reader.runs:
+      for event in reader.runs[run]:
+        if event<1200: print("event: ", event)
+        cameras = get_response_data(reader.runs[run][event])
+        truth = get_truth_data(edepFile, geom, event)
+        events.append(cameras)
+        truths.append(truth)
+  
+  events_data = np.stack(events, axis=0)
+  truths_data = np.stack(truths, axis=0)
+  print("events:", events_data.shape, events_data.nbytes / 1024 / 1024)
+  print("truths:", truths_data.shape, truths_data.nbytes / 1024 / 1024)
+
+  # Save to HDF5 file
+  with h5py.File('/home/filippo/DUNE/data/numu-CC-QE/dataset.h5', 'w') as f:
+    f.create_dataset('inputs', data=events_data, compression='gzip')
+    f.create_dataset('targets', data=truths_data, compression='gzip')
