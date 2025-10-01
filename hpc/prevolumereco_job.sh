3a49e25a90e094cb25fa0b6a33d4971ef24eb77f
Filippo Mei <fmeihpc@ui-terabit.cr.cnaf.infn.it>
2025-10-01 12:07:39 +0200

    fix

--- hpc/prevolumereco_job.sh
+++ hpc/prevolumereco_job.sh
@@ -7,5 +7,5 @@
 #SBATCH --error=/storage-hpc/ntosi/SAND-LAr-PROD/GRAIN/numu_CC_QE_Filippo/prevolumereco/logs/prevolumereco.err
 
 source /storage-hpc/ntosi/SAND-LAr-BIN/pre-volumereco-venv/bin/activate
-srun /home/HPC/fmeihpc/scripts/pre-volumereco/hpc/train_hpc.py
+srun python3 /home/HPC/fmeihpc/scripts/pre-volumereco/hpc/train_hpc.py /home/HPC/fmeihpc/scripts/pre-volumereco/data/lightweight_dataset_20cm.h5 --output_dir /storage-hpc/ntosi/SAND-LAr-PROD/GRAIN/numu_CC_QE_Filippo/prevolumereco --model_name test
 deactivate
